e054244bbc68104fb28a9922331993459defafed
wendy-medina <endyleyms@gmail.com>
2022-04-15 04:30:39 -0500

    Adelanto assesment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+import "./Navbar.css"
+
+function Navbar (){
+    return(
+        <div className="Navbar">
+            <NavLink className="letter" to="/">Home</NavLink>
+            <NavLink className="letter" to= "/about">About</NavLink>
+        </div>
+    );
+}
+
+
+export default Navbar;
