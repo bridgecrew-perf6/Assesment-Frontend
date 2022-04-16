7a1457e9cc08ad46b876d09906a8ed099944b292
wendy-medina <endyleyms@gmail.com>
2022-04-15 19:35:41 -0500

    countdown agregate

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,8 +8,8 @@ function About (){
         <h1>Bienveni@, mi nombre es Wendy Medina</h1>
         <div className="contenedorimgtext"><img className="fotowendy" src={image} alt=""/>
             <div className="contenerdortext">
-                <p>Soy Bióloga hace aproximadamente 3 años, con experiencia en el area de investigación en biotecnologia vegetal. Me encantan las plantas, en especial las orquídeas y las aves</p>
-                <p>Desde hace un tiempo me ha venido interesando el area del desarrollo web, por tal motivo decidí cambiar de carrera, o quizas fusionar mi carrera con el desarrollo web, porque creo que esta area tiene mucho potencial para poder tener mayor alcance en la ciencia</p>
+                <p>Soy Bióloga hace aproximadamente 3 años, con experiencia en el area de investigación en biotecnologia vegetal. Me encantan las plantas, en especial las orquídeas y  también las aves</p>
+                <p>Desde hace un tiempo me he venido interesando el area del desarrollo web, por tal motivo decidí cambiar de carrera, o quizas fusionar mi carrera con el desarrollo web, ya que creo que esta area tiene mucho potencial para poder tener mayor alcance en la ciencia</p>
                 
                 <ul>
                     Durante la trayectoria de aprendizaje realizada en el camp del TOP de MAKE IT REAL, he podido generar nuevos conocimientos en:
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import  { React, useState } from "react";
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Countdown from 'react-countdown';
 import "./ProductCard.css";
 
 
 function ProductCard ({id, title, image}){
-    
+    const [countdown, setCountdown] = useState(true);
 
+    const handlerCompleted = () => {
+        setCountdown(false);
+      };
 
 
     return(
         <div className="card">
-            <Link to={`/ProductDetail/${id}`}>
+            <Link className={countdown ? 'link__active' : 'link__inactive'} to={`/ProductDetail/${id}`}>
                 <img className="imagecard" src={image} alt=""/>
                 <p className="titleproduct">{title}</p>
+                <Countdown  className="countdown" date={Date.now() + parseInt((Math.random() * (300000)), 10)} onComplete={handlerCompleted}/>
                  
             </Link>
         </div>
